9250ba98f9ca5ebd2322e58a7e1c8e2a4388bfed
Evan Grinde <evan.grinde@gmail.com>
2021-08-09 21:10:34 -0400

    add hosts

--- development/strapi-config/config/middleware.js
+++ development/strapi-config/config/middleware.js
@@ -2,7 +2,11 @@ module.exports = () => ({
   settings: {
     cors: {
       enabled: true,
-      origin: [process.env.ADMIN_HOST, process.env.CLIENT_HOST],
+      origin: [
+        process.env.ADMIN_HOST,
+        process.env.CLIENT_ROOTED_HOST,
+        process.env.CLIENT_ROOTLESS_HOST,
+      ],
     },
   },
 });
