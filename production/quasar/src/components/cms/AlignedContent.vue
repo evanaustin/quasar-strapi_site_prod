37f565197082eea4d2489452756d4ea9aa813df6
Evan Grinde <evan.grinde@gmail.com>
2021-08-09 20:49:17 -0400

    add components/cms/alignedcontent

---
+++ production/quasar/src/components/cms/AlignedContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="`text-${alignedContent.alignment}`">
+    <h4 v-if="alignedContent.title">
+      {{ alignedContent.title }}
+    </h4>
+
+    <h6 v-if="alignedContent.subtitle">
+      {{ alignedContent.subtitle }}
+    </h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlignedContent",
+  props: {
+    alignedContent: {
+      type: Object,
+    },
+  },
+  computed: {
+    strapiURL() {
+      return process.env.STRAPI_URL;
+    },
+  },
+};
+</script>
