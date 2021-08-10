e2db9b5416fb563f3313560186af48f68af116dc
Evan Grinde <evan.grinde@gmail.com>
2021-08-09 21:09:59 -0400

    refactor components/cms/copy

--- development/quasar/src/components/cms/Copy.vue
+++ development/quasar/src/components/cms/Copy.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-      <!-- {{ copy }} -->
+  <div :class="classes">
     <Markdown>{{ copy }}</Markdown>
   </div>
 </template>
@@ -11,8 +10,13 @@ import VueMarkdown from "vue-markdown";
 export default {
   name: "Copy",
   props: {
+    styles: {
+      type: Object,
+      default: null,
+    },
     copy: {
       type: String,
+      default: "",
     },
   },
   components: {
@@ -21,6 +25,22 @@ export default {
   computed: {
     strapiURL() {
       return process.env.STRAPI_URL;
+    },
+    alignment() {
+      if (!this.styles) return false;
+      return this.styles.alignment ? `text-${this.styles.alignment}` : "";
+    },
+    padding() {
+      if (!this.styles) return false;
+      
+      const px = this.styles.padding_left_right;
+      const py = this.styles.padding_top_bottom;
+
+      return (px ? `q-px-${px}` : "") + (px && py ? " " : "") + (py ? `q-py-${py}` : "");
+    },
+    classes() {
+      if (!this.styles) return false;
+      return this.alignment + (this.alignment && this.padding ? " " : "") + this.padding;
     },
   },
 };
